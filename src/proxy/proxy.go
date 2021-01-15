af58976283c709b91ea7bb26c53104cfbc2d916b
alexthemonk <liushucheng9661@gmail.com>
2021-01-15 10:32:57 -0600

    test

--- src/proxy/proxy.go
+++ src/proxy/proxy.go
@@ -15,9 +15,9 @@ func main() {
 	// For testing purposes, the port is set to a higher number to avoid sudo
 	// client.Init("127.0.0.1", 53533)
 	signal.Notify(client.ShutDownChan, syscall.SIGINT, syscall.SIGTERM)
-	// client.AddUpstream("Google", "8.8.8.8/resolve", 443)       // dns.google.com
-	// client.AddUpstream("Cloudflare", "1.1.1.1/dns-query", 443) // cloudflare-dns.com
-	// client.AddUpstream("Quad9", "9.9.9.9:5053/dns-query", 443) // dns.quad9.net
+	client.AddUpstream("Google", "8.8.8.8/resolve", 443)       // dns.google.com
+	client.AddUpstream("Cloudflare", "1.1.1.1/dns-query", 443) // cloudflare-dns.com
+	client.AddUpstream("Quad9", "9.9.9.9:5053/dns-query", 443) // dns.quad9.net
 	client.AddUpstream("Google", "8.8.8.8", 53)
 
 	client.StartProxy()
